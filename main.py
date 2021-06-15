0c2728b0360d840e9d26779e682273b387fccfb8
Miguel <guerrero.cervan@gmail.com>
2021-06-16 00:03:05 +0200

    Prueba de música y marcador que cuenta puntos

--- main.py
+++ main.py
@@ -1,23 +1,44 @@
 import pygame
 import random
+from pygame import mixer
+from pygame import sprite
 
 from pygame.constants import *
 
-
+FPS = 60
 ANCHO = 800
 ALTO = 600
 NEGRO = (0, 0, 0)
 BLANCO = (255, 255, 255)
 
+
+# Inicio del juego y crear la pantalla
 pygame.init()
 pantalla = pygame.display.set_mode((ANCHO, ALTO))
 pygame.display.set_caption("Space Trooper Adventure")
 clock = pygame.time.Clock()
+pygame.mixer.music.load("audio/metroid.wav")
+pygame.mixer.music.set_volume(0.2)
 
+#Por ahora voy a dejar el marcador muy normalito
+class Marcador(pygame.sprite.Sprite):
+    def __init__(self, x, y, fontsize=40, color=BLANCO):
+        super().__init__()
+        self.fuente = pygame.font.SysFont("Namco", fontsize)
+        self.texto = 0
+        self.color = color
+        self.image = self.fuente.render(str(self.texto), True, self.color)
+        self.rect = self.image.get_rect()
+    
+    def update(self):
+        self.image = self.fuente.render(str(self.texto), True, self.color)
+
+# El jugador que controla la nave
 class Nave(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load("images/nave.png").convert()
+        self.image = pygame.transform.scale(self.image, (50, 60))
         self.image.set_colorkey(NEGRO)
         self.rect = self.image.get_rect()
         self.rect.centerx = ANCHO - 750
@@ -37,6 +58,7 @@ class Nave(pygame.sprite.Sprite):
         if self.rect.bottom > ALTO:
             self.rect.bottom = ALTO
 
+#Los meteoritos
 class Roca(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -47,6 +69,7 @@ class Roca(pygame.sprite.Sprite):
         self.rect.x = random.randrange(780, 800)
         self.rect.y = random.randrange(ALTO - self.rect.height)
         self.vx = random.randrange(1, 10)
+
     
     def update(self):
         self.rect.x -= self.vx
@@ -54,12 +77,16 @@ class Roca(pygame.sprite.Sprite):
             self.rect.x = random.randrange(780, 800)
             self.rect.y = random.randrange(ALTO - self.rect.height)
             self.vx = random.randrange(1, 10)
+            marcador.texto += 1
 
+# Fondo de la pantalla 
 fondo = pygame.image.load("images/fondo.jpg").convert()
+#Hacer que el fondo sea dinámico si me da tiempo (estrellitas brillando, que se mueva, ¿que haga cosas chulas?)
 
+# Incluir los objetos en el Group
 grupoSprites = pygame.sprite.Group()
 roca_list = pygame.sprite.Group()
-for i in range(20):
+for i in range(15):
     roca = Roca()
     grupoSprites.add(roca)
     roca_list.add(roca)
@@ -67,6 +94,12 @@ for i in range(20):
 nave = Nave()
 grupoSprites.add(nave)
 
+marcador = Marcador(10, 10)
+grupoSprites.add(marcador)
+
+pygame.mixer.music.play(loops=-1)
+
+# Bucle Principal
 running = True
 while running:
     clock.tick(60)
@@ -76,6 +109,12 @@ while running:
     
     grupoSprites.update()
 
+# Si hay una colisión
+    colision = pygame.sprite.spritecollide(nave, roca_list, True)
+    if colision:
+        running = False
+
+# Blit, Draw, Displays
     pantalla.blit(fondo, [0, 0])
 
     grupoSprites.draw(pantalla)
